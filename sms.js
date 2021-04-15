4da32bbea90e6ab5b24b1311f4d174de7562f4a8
Morgan Franke <morganleighfranke@gmail.com>
2021-04-14 23:01:00 -0400

    twilio folder with server.js

--- sms.js
+++ sms.js
@@ -1,23 +1,29 @@
+require("dotenv").config();
 // Download the helper library from https://www.twilio.com/docs/node/install
 // Your Account Sid and Auth Token from twilio.com/console
 // and set the environment variables. See http://twil.io/secure
-require('dotenv').config();
 
-const accountSid = process.env.TWILIO_ACCOUNT_SID;
-const authToken = process.env.TWILIO_AUTH_TOKEN;
-const client = require('twilio')(accountSid, authToken);
+const sms = () => {
+  console.log("itworks");
+  console.log(process.env.TWILIO_ACCOUNT_SID);
+  const accountSid = process.env.TWILIO_ACCOUNT_SID;
+  const authToken = process.env.TWILIO_AUTH_TOKEN;
+  const client = require("twilio")(accountSid, authToken);
 
-var twilio = require('twilio');
+  var twilio = require("twilio");
 
-var numbersToMessage = ["+17203656996", "+17204251946"]
+  var numbersToMessage = ["+17203656996", "+17204251946"];
+  console.log("thisworks too");
+  numbersToMessage.forEach(function (number) {
+    var message = client.messages
+      .create({
+        body: "Time to log your childs sleep chart!",
+        from: "+17326075490",
+        to: number,
+      })
+      .then((message) => console.log(message.status))
+      .done();
+  });
+};
 
-numbersToMessage.forEach(function(number){
-  var message = client.messages.create({
-    body: 'Time to log your childs sleep chart!',
-    from: '+17326075490',
-    to: number
-  })
-  .then(message =>  console.log(message.status))
-  .done();
-});
-
+module.exports = sms;
