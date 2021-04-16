5efda2a3317fcdc86b5b5eeee6178d8881438fd1
Morgan Franke <morganleighfranke@gmail.com>
2021-04-16 14:30:07 -0400

    twilio is FINALLY WORKINGGGGGGGG

--- Dailytext/server.js
+++ Dailytext/server.js
@@ -1,18 +1,16 @@
 const express = require("express");
-const { getExpectedTwilioSignature } = require("twilio/lib/webhooks/webhooks");
 const app = express();
 const sms = require("../sms");
 require('dotenv').config();
-// var twilio = require('twilio');
 
 const PORT = process.env.PORT || 3002;
 
 var now = new Date();
-var millisTill8 = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 13, 0, 0, 0) - now;
+var millisTill8 = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 20, 0, 0, 0) - now;
 console.log(millisTill8);
 if (millisTill8 < 0) {
-    console.log("if statement works");
-     millisTill8 += 86400000; // it's after 10am, try 10am tomorrow.
+   
+     millisTill8 += 86400000; // it's after 8am, try 8am tomorrow.
 }
 setTimeout(function(){sms()}, millisTill8);
 
--- sms.js
+++ sms.js
@@ -1,15 +1,11 @@
 require("dotenv").config();
-// require('dotenv').load();
 const sms = () => {
 
-  // console.log(process.env.TWILIO_ACCOUNT_SID);
-//   const accountSid = process.env.TWILIO_ACCOUNT_SID;
-//   const authToken = process.env.TWILIO_AUTH_TOKEN;\
-    const accountSid = 'ACd00ec84090b687fc61fc0a004439951d';
-    const authToken = 'e92008b63ea5f872f59432fa56a78f9e';
+
+  const accountSid = process.env.TWILIO_ACCOUNT_SID;
+  const authToken = process.env.TWILIO_AUTH_TOKEN;
   const client = require("twilio")(accountSid, authToken);
-//   var twilio = require("twilio");
-// var twilio = require('twilio')(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_TOKEN);
+
   var numbersToMessage = ["+17203656996"];
   //rob , "+17204251946"
 
