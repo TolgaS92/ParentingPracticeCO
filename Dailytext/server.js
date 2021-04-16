b295fa9ef40e4581982cab40a8e9faecdb06643b
Tolga Secme <tolgasecme@icloud.com>
2021-04-16 12:37:54 -0600

    new push

--- Dailytext/server.js
+++ Dailytext/server.js
@@ -1,44 +1,17 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const sms = require("../sms");
+const sms = require('../sms');
 require('dotenv').config();
 
 const PORT = process.env.PORT || 3002;
 
-// function test() {
-//     var date = new Date();
-//     if(date.getDate()==1 && date.getHours()+1 >= 5 && date.getMinutes()+1 >= 8) {
-//         sms();
-//         clearInterval();
-//     }
-//     console.log('it works');
-// }
-
-// test();
-
-//daily milliseconds 86400000
-
-setInterval(() => {
-    //add database call 
-
-    //when a number comes in from database check if it has a + and a 1, if true run function, else add + or 1 or both
-    //send everyday at 8pm MST
-    //check to see if it is 8pm then reset it
-
-    // var now = new Date();
-    // var millisTill8 = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 17, 0, 0, 0) - now;
-    // if (millisTill8 < 0) {
-    //      millisTill8 += 86400000; // it's after 8pm, try 8pm tomorrow.
-    // }
-    // setTimeout(sms(), millisTill8);
-    var date = new Date();
-    if (date.getDay()==1 && date.getHours()+1 >= 6){
-        sms();
-        console.log('it works');
-    };
-}, 1000 );
+var now = new Date();
+var millisTill8 = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 20, 0, 0, 0) - now;
+console.log(millisTill8);
+if (millisTill8 < 0) {
+   
+     millisTill8 += 86400000; // it's after 8am, try 8am tomorrow.
+}
+setTimeout(function(){sms()}, millisTill8);
 
 app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
-}, 60000);
-app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
-
--- sms.js
+++ sms.js
@@ -1,22 +1,21 @@
-require("dotenv").config();
-// require('dotenv').load();
+require('dotenv').config();
+// require(‘dotenv’).load();
 const sms = () => {
-
-  console.log(process.env.TWILIO_ACCOUNT_SID);
-//   const accountSid = process.env.TWILIO_ACCOUNT_SID;
-//   const authToken = process.env.TWILIO_AUTH_TOKEN;\
-    const accountSid = 'ACd00ec84090b687fc61fc0a004439951d';
-    const authToken = '96aab27b7bff485bdbdec5eab5a4d28b';
-  const client = require("twilio")(accountSid, authToken);
-//   var twilio = require("twilio");
-// var twilio = require('twilio')(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_TOKEN);
-  var numbersToMessage = ["+17203656996", "+17204251946"];
-
+ console.log(process.env.TWILIO_ACCOUNT_SID);
+  const accountSid = process.env.TWILIO_ACCOUNT_SID;
+  const authToken = process.env.TWILIO_AUTH_TOKEN;
+    /* const accountSid = 'ACd00ec84090b687fc61fc0a004439951d';
+    const authToken = 'e92008b63ea5f872f59432fa56a78f9e'; */
+  const client = require('twilio')(accountSid, authToken);
+//   var twilio = require('twilio');
+// var twilio = require(‘twilio’)(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_TOKEN);
+  var numbersToMessage = ['+17203656996'];
+  //rob , '+17204251946'
   numbersToMessage.forEach(function (number) {
     var message = client.messages
       .create({
-        body: "Time to log your childs sleep chart!",
-        from: "+17326075490",
+        body: 'Time to log your childs sleep chart!',
+        from: '+17326075490',
         to: number,
       })
       .then((message) => console.log(message.status))
